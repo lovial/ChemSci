331d755fe00672d62eb8253ac4619e246646e3c9
lovial <laurent.vial@ymail.com>
2020-06-24 10:16:39 +0200

    Add files via upload

--- Avancement.m
+++ Avancement.m
@@ -5,21 +5,22 @@ function [Result] = Avancement(G0,K,Hi,Dmax)
 Nb_Pts = size(G0,1);
 Residuals = zeros(Nb_Pts,5); 
 Result = zeros(Nb_Pts,5);
-Constants = K(1:4); % Association constants
-DHG = K(5:9); %Ratio of induced 1H NMR signal displacement upon second binding event by that of first binding event.
+Constants = K(1:5); % Association constants
+DHG = K(6:11); %Ratio of induced 1H NMR signal displacement upon second binding event by that of first binding event.
 
 for i = 1:Nb_Pts
     X = Function (G0(i),Constants,Hi);
     Residuals(i,1:5)= X(1:5); % Concentration of unbound species.
-%%  Calculated 1H NMR signal displacement of pR)3/(pS)3
+%%  Calculated 1H NMR signal displacement of (pR)3/(pS)3
     Result(i,1) = Dmax(1)*(Hi(2)-Residuals(i,2))/Hi(2);
 %%  Calculated displacement of the four 1H NMR Signals of (pR)3pS / (pS)3pR
     Result(i,2) = Dmax(2)*(Constants(3)*Constants(2)*Residuals(i,3)*Residuals(i,5)^2)/Hi(3)+(DHG(1)*Constants(2)*Residuals(i,3)*Residuals(i,5))/Hi(3);
     Result(i,3) = Dmax(3)*(Constants(3)*Constants(2)*Residuals(i,3)*Residuals(i,5)^2)/Hi(3)+(DHG(2)*Constants(2)*Residuals(i,3)*Residuals(i,5))/Hi(3);
     Result(i,4) = Dmax(4)*(Constants(3)*Constants(2)*Residuals(i,3)*Residuals(i,5)^2)/Hi(3)+(DHG(3)*Constants(2)*Residuals(i,3)*Residuals(i,5))/Hi(3);
     Result(i,5) = Dmax(5)*(Constants(3)*Constants(2)*Residuals(i,3)*Residuals(i,5)^2)/Hi(3)+(DHG(4)*Constants(2)*Residuals(i,3)*Residuals(i,5))/Hi(3);
-%%  Calculated 1H NMR signal displacement of(pR)3pS / (pS)3pR
-    Result(i,6) = Dmax(6)*(Hi(4)-Residuals(i,4))/Hi(4);
+%%  Calculated 1H NMR signal displacement of(pR)2(pS)2 
+    Result(i,6) = Dmax(6)*(Constants(4)*Constants(5)*Residuals(i,4)*Residuals(i,5)^2)/Hi(4)+(DHG(5)*Constants(4)*Residuals(i,4)*Residuals(i,5))/Hi(4);
+    Result(i,7) = Dmax(7)*(Constants(4)*Constants(5)*Residuals(i,4)*Residuals(i,5)^2)/Hi(4)+(DHG(6)*Constants(4)*Residuals(i,4)*Residuals(i,5))/Hi(4);
 end
 
 
--- General.m
+++ General.m
@@ -1,16 +1,16 @@
 %% General script for the concomitant fitting of association constants, as 
 %%in "The dark side of disulfide-based dynamic combinatorial chemistry" 
 %%from L. Vial et al.
-%%Example is given for 1:1 associations stoechiometries for (pR)4/(pS)4,
-%%pR)3/(pS)3 and (pR)2(pS)2 pairing with cadaverine and 1:2 pairing of (pR)3pS / (pS)3pR with cadaverine
-%%fz
+%%Example is given for 1:1 associations stoechiometries for (pR)4/(pS)4 and
+%%(pR)3/(pS)3 pairing with cadaverine and 1:2 pairing of (pR)3pS / (pS)3pR and (pR)2(pS)2 with cadaverine
+%%
 
 
 %% Let's get some data
 G0 = DATA(:,1); % matrix ov Guest concentration (M)
-HData = DATA(:,2:7); %matrix of 1H NMR signal displacement (ppm)
+HData = DATA(:,2:8); %matrix of 1H NMR signal displacement (ppm)
 H0 = 9.13 * 10^-3 ; %M , Initial Host concentration
-Repartition= [30;40;20;10]; % Molar fraction of A4-C4, A3-C3, A4-C1, A4-H in Host pool respectively
+Repartition= [30;40;20;10]; % Molar fraction of (pR)4/(pS)4, (pR)3/(pS)3, (pR)3pS / (pS)3pR, (pR)2(pS)2 in Host pool respectively
 
 Hi = Repartition (:,1)*H0/100 ;% Initial concentration in each host species
 
@@ -22,8 +22,8 @@ end
 %% Parameter Fitting
 
 fun = @(K,G0) Avancement(G0,K,Hi,Dmax);
-Constants0 = [ 10^2; 10^2;10^2; 5*10^4; 1;1;1;1;1]; %Initiation of parameters. Here, the first four are association costants, the subsequents are the ratio of induced 1H NMR signal displacement upon second binding event by that of first binding event.
-LB = [0;0;0;0;0;0;0;0;0]; %Lower boundaries of parameters
+Constants0 = [ 10^2; 10^2;10^2; 10^2;10^2;1;1;1;1;1;1]; %Initiation of parameters. Here, the first five are association costants, the subsequents are the ratio of induced 1H NMR signal displacement upon second binding event by that of first binding event.
+LB = [0;0;0;0;0;0;0;0;0;0;0]; %Lower boundaries of parameters
 [Kres, resnorm] = lsqcurvefit(fun,Constants0,G0,HData, LB); % Kres contains the fitting parameters after fitting
 
 %% Calcul of the coefficient of determination
