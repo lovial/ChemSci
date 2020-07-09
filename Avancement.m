b5cecc0fd49a22599209cc9b61582e123a714e1e
lovial <laurent.vial@ymail.com>
2020-07-09 09:27:27 +0200

    Add files via upload

--- Avancement.m
+++ Avancement.m
@@ -1,4 +1,4 @@
-function [Result] = Avancement(G0,K,Hi,Dmax)
+function [Result] = Avancement(G0,K,Hi)
 %% For a set of parameters, calculate the expected 1H NMR signal displacement for each species.
 
 
@@ -6,21 +6,32 @@ Nb_Pts = size(G0,1);
 Residuals = zeros(Nb_Pts,5); 
 Result = zeros(Nb_Pts,5);
 Constants = K(1:5); % Association constants
-DHG = K(6:11); %Ratio of induced 1H NMR signal displacement upon second binding event by that of first binding event.
+DHG = K(6:19); %Ratio of induced 1H NMR signal displacement upon second binding event by that of first binding event.
 
 for i = 1:Nb_Pts
     X = Function (G0(i),Constants,Hi);
     Residuals(i,1:5)= X(1:5); % Concentration of unbound species.
 %%  Calculated 1H NMR signal displacement of (pR)3/(pS)3
-    Result(i,1) = Dmax(1)*(Hi(2)-Residuals(i,2))/Hi(2);
+    Result(i,1) = (Hi(2)-Residuals(i,2))/Hi(2);
+    Result(i,2) = (Hi(2)-Residuals(i,2))/Hi(2);
+
 %%  Calculated displacement of the four 1H NMR Signals of (pR)3pS / (pS)3pR
-    Result(i,2) = Dmax(2)*(Constants(3)*Constants(2)*Residuals(i,3)*Residuals(i,5)^2)/Hi(3)+(DHG(1)*Constants(2)*Residuals(i,3)*Residuals(i,5))/Hi(3);
-    Result(i,3) = Dmax(3)*(Constants(3)*Constants(2)*Residuals(i,3)*Residuals(i,5)^2)/Hi(3)+(DHG(2)*Constants(2)*Residuals(i,3)*Residuals(i,5))/Hi(3);
-    Result(i,4) = Dmax(4)*(Constants(3)*Constants(2)*Residuals(i,3)*Residuals(i,5)^2)/Hi(3)+(DHG(3)*Constants(2)*Residuals(i,3)*Residuals(i,5))/Hi(3);
-    Result(i,5) = Dmax(5)*(Constants(3)*Constants(2)*Residuals(i,3)*Residuals(i,5)^2)/Hi(3)+(DHG(4)*Constants(2)*Residuals(i,3)*Residuals(i,5))/Hi(3);
+    Result(i,3) =(Constants(3)*Constants(2)*Residuals(i,3)*Residuals(i,5)^2)/Hi(3)+(DHG(1)*Constants(2)*Residuals(i,3)*Residuals(i,5))/Hi(3);
+    Result(i,4) = (Constants(3)*Constants(2)*Residuals(i,3)*Residuals(i,5)^2)/Hi(3)+(DHG(2)*Constants(2)*Residuals(i,3)*Residuals(i,5))/Hi(3);
+    Result(i,5) = (Constants(3)*Constants(2)*Residuals(i,3)*Residuals(i,5)^2)/Hi(3)+(DHG(3)*Constants(2)*Residuals(i,3)*Residuals(i,5))/Hi(3);
+    Result(i,6) = (Constants(3)*Constants(2)*Residuals(i,3)*Residuals(i,5)^2)/Hi(3)+(DHG(4)*Constants(2)*Residuals(i,3)*Residuals(i,5))/Hi(3);
+    Result(i,7) = (Constants(3)*Constants(2)*Residuals(i,3)*Residuals(i,5)^2)/Hi(3)+(DHG(5)*Constants(2)*Residuals(i,3)*Residuals(i,5))/Hi(3);
+    Result(i,8) = (Constants(3)*Constants(2)*Residuals(i,3)*Residuals(i,5)^2)/Hi(3)+(DHG(6)*Constants(2)*Residuals(i,3)*Residuals(i,5))/Hi(3);
+    Result(i,9) = (Constants(3)*Constants(2)*Residuals(i,3)*Residuals(i,5)^2)/Hi(3)+(DHG(7)*Constants(2)*Residuals(i,3)*Residuals(i,5))/Hi(3);
+    Result(i,10) = (Constants(3)*Constants(2)*Residuals(i,3)*Residuals(i,5)^2)/Hi(3)+(DHG(8)*Constants(2)*Residuals(i,3)*Residuals(i,5))/Hi(3);
+
+    
 %%  Calculated 1H NMR signal displacement of(pR)2(pS)2 
-    Result(i,6) = Dmax(6)*(Constants(4)*Constants(5)*Residuals(i,4)*Residuals(i,5)^2)/Hi(4)+(DHG(5)*Constants(4)*Residuals(i,4)*Residuals(i,5))/Hi(4);
-    Result(i,7) = Dmax(7)*(Constants(4)*Constants(5)*Residuals(i,4)*Residuals(i,5)^2)/Hi(4)+(DHG(6)*Constants(4)*Residuals(i,4)*Residuals(i,5))/Hi(4);
+    Result(i,11) = (Constants(4)*Constants(5)*Residuals(i,4)*Residuals(i,5)^2)/Hi(4)+(DHG(9)*Constants(4)*Residuals(i,4)*Residuals(i,5))/Hi(4);
+    Result(i,12) = (Constants(4)*Constants(5)*Residuals(i,4)*Residuals(i,5)^2)/Hi(4)+(DHG(10)*Constants(4)*Residuals(i,4)*Residuals(i,5))/Hi(4);
+    Result(i,13) = (Constants(4)*Constants(5)*Residuals(i,4)*Residuals(i,5)^2)/Hi(4)+(DHG(11)*Constants(4)*Residuals(i,4)*Residuals(i,5))/Hi(4);
+    Result(i,14) = (Constants(4)*Constants(5)*Residuals(i,4)*Residuals(i,5)^2)/Hi(4)+(DHG(12)*Constants(4)*Residuals(i,4)*Residuals(i,5))/Hi(4);
+
 end
 
 
--- General.m
+++ General.m
@@ -8,7 +8,7 @@
 
 %% Let's get some data
 G0 = DATA(:,1); % matrix ov Guest concentration (M)
-HData = DATA(:,2:8); %matrix of 1H NMR signal displacement (ppm)
+HData = DATA(:,2:15); %matrix of 1H NMR signal displacement (ppm)
 H0 = 9.13 * 10^-3 ; %M , Initial Host concentration
 Repartition= [30;40;20;10]; % Molar fraction of (pR)4/(pS)4, (pR)3/(pS)3, (pR)3pS / (pS)3pR, (pR)2(pS)2 in Host pool respectively
 
@@ -17,46 +17,45 @@ Hi = Repartition (:,1)*H0/100 ;% Initial concentration in each host species
 for i = 1:size(HData,2)
     Dmax(i) = max(HData(:,i)); %Maximal 1H NMR signal displacement
 end
-
-
+HDataNorm = HData./Dmax;
+Kres(1:5)
 %% Parameter Fitting
 
-fun = @(K,G0) Avancement(G0,K,Hi,Dmax);
-Constants0 = [ 10^2; 10^2;10^2; 10^2;10^2;1;1;1;1;1;1]; %Initiation of parameters. Here, the first five are association costants, the subsequents are the ratio of induced 1H NMR signal displacement upon second binding event by that of first binding event.
-LB = [0;0;0;0;0;0;0;0;0;0;0]; %Lower boundaries of parameters
-[Kres, resnorm] = lsqcurvefit(fun,Constants0,G0,HData, LB); % Kres contains the fitting parameters after fitting
-
+fun = @(K,G0) Avancement(G0,K,Hi);
+Constants0 = [ 10^2; 10^2;10^2; 10^2;10^2;1;1;1;1;1;1;1;1;1;1;1;1;1;1]; %Initiation of parameters. Here, the first five are association costants, the subsequents are the ratio of induced 1H NMR signal displacement upon second binding event by that of first binding event.
+LB = [0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0]; %Lower boundaries of parameters
+[Kres, resnorm] = lsqcurvefit(fun,Constants0,G0,HDataNorm, LB); % Kres contains the fitting parameters after fitting
+HResult = fun(Kres,G0).*Dmax;
 %% Calcul of the coefficient of determination
-
-Moyenne = mean(mean(HData));
-
-Somme = 0;
-for i = 1:size(HData,2)
-    for j=1:size(HData,1)
-        Somme = Somme + (HData(j,i)-Moyenne)^2;
+Coeff_Deter = zeros(size(HData,2),1);
+for j =1:size(HData,2)
+    Somme = 0;
+    e = 0;
+    Mean = mean(HData(:,j));
+    for i = 1:size(HData,1)
+        e = e + (HResult(i,j)-HData(i,j))^2;
+        Somme = Somme + (HData(i,j)-MoyenneA3)^2;
     end
+    Coeff_Deter(j)=1-e/Somme;
+   
 end
+Coeff_Deter
 
-R2 = 1- resnorm/Somme % Coefficient of determination
 
 %% Plotting
 
-HResult = fun(Kres,G0);
+figure
+for i = 1:size(HData,2)
+    subplot(4,4,i)
+    plot(G0,HData(:,i),'+')
+    hold on
+    plot(G0,HResult(:,i),'-')
+end
 figure
 
-subplot(2,2,1:2)
 plot(G0,HData,'+')
 hold on
 set(gca,'ColorOrderIndex',1)
 plot(G0,HResult,'-')
 
-subplot(2,2,3)
-plot(G0,HData,'+')
-hold on
-set(gca,'ColorOrderIndex',1)
-plot(G0,HResult,'-')
 
-subplot(2,2,4)
-hold on
-plot(G0,HData(:,6),'+','Color',[0.3010,0.7450,0.9330])
-plot(G0,HResult(:,6),'-','Color',[0.3010,0.7450,0.9330])
